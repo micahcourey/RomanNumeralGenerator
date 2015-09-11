1641232faf77d6565bbb0a7c7024d8714c97c4a1
micahcourey <micahjcourey@gmail.com>
2015-09-10 18:42:32 -0700

    add readme file and change function to only allow roman number results up to 3999

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,20 @@
 var romanNumeral = function(number) {
-
-  var list = { "M":1000,
-              "CM":900,
-               "D":500,
-              "CD":400,
-               "C":100,
-              "XC":90,
-               "L":50,
-              "XL":40,
-               "X":10,
-              "IX":9,
-               "V":5,
-              "IV":4,
-               "I":1
-              };
+  if(number > 3999 || number < 1) {
+    return "not a number please enter a number between 1 and 3999";
+  } else {
+    var list = { "M":1000, "CM":900, "D":500, "CD":400, "C":100, "XC":90,
+                 "L":50, "XL":40, "X":10, "IX":9, "V":5, "IV":4, "I":1 };
     var roman = '';
     var i;
-
-    for ( i in list ) {
-      while ( number >= list[i] ) {
-        roman += i;
-        number -= list[i];
-     }
+      for ( i in list ) {
+        while ( number >= list[i] ) {
+          roman += i;
+          number -= list[i];
+        }
+      }
     }
     return roman;
-}
+};
 
 $(document).ready(function() {
   $("form#countupby").submit(function(event) {
